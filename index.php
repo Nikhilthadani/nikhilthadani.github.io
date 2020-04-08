c1a4ca35e8f87d9c999631e007063bdf41f7ec2f
Nikhilthadani <63336772+Nikhilthadani@users.noreply.github.com>
2020-04-08 14:13:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.js">
+
+    <title>Home</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active"  href="index.php">Home</a>
+            <a class="nav-link" href="feedback.php">Feedback</a>
+            <a class="nav-link" href="contact.php">Contact</a>
+          </nav> <hr>
+          <h1 class="masthead-brand" >Projects Hub</h1>
+        </div>
+      </header>
+
+      <main role="main" class="inner cover">
+        <h3 class="cover-heading" align="left">Welcome to my Page.</h3> <hr>
+        <p class="lead"><i>Hello Folks. We are providing Projects for students who are beginners in programming. Get some from projects here. And please reffer your friends about this page. <br> Thank You</i></p>
+        <p class="lead">
+
+          <a href="tech.php" class="btn btn-lg btn-secondary">Technical Projects</a>
+          <a href="nontech.php" class="btn btn-lg btn-secondary">Non-Technical Projects</a>
+        </p>
+
+
+      </main>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Made By Nikhil Thadani <br> Join us on  <a href="t.me/ProjectsHub">Telegram</a></p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ nontech.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <link rel="stylesheet" href="bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap.min.js">
+
+    <title>projects</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/cover/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+          
+          <nav class="nav nav-masthead justify-content-center">
+            <a class="nav-link active"  href="index.php">Home</a>
+            <a class="nav-link" href="fdbk.php">Feedback</a>
+            <a class="nav-link" href="contact.php">Contact</a>
+          </nav> <hr>
+          <h1 class="masthead-brand" >Projects Hub</h1>
+        </div>
+      </header>
+
+      <h3>coming soon</h3>
+
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+          <p>Made By Nikhil Thadani <br> Join us on  <a href="t.me/ProjectsHub">Telegram</a></p>
+        </div>
+      </footer>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
